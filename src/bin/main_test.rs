b95d2d2cebe3027a729c7214a06238386f1e13d1
Vimukthi Herath <vimukthibh96@gmail.com>
2024-03-22 22:34:16 +1100

    added main entry testing point

---
+++ src/bin/main_test.rs
@@ -0,0 +1,40 @@
+use std::io::ErrorKind;
+mod lex_test;
+mod parse_test;
+mod evaluator_test;
+use lex_test::lexer;
+use parse_test::parse;
+use evaluator_test::Evaluator;
+
+
+
+fn main() { 
+
+    let file_path = "./src/test.lg";
+   
+    // Generate Tokens, manage errors
+    let tokens = match lexer(file_path) {
+        Ok(tokens) => tokens,
+        Err(e) => {
+            match e.kind() {    
+                ErrorKind::NotFound => panic!("Error: File not found"),
+                ErrorKind::PermissionDenied => panic!("Error: Permission to file denied"),
+                ErrorKind::InvalidData => panic!("Nnvalid (non utf-8) character encountered file"),
+                // Generic handling of other IO errors
+                _ => panic!("Error: {}", e),
+            }
+        }
+    };
+ 
+    // Parse & generate AST
+    let ast = match parse(tokens) {
+        Ok(ast) => ast,
+        Err(e) => panic!("Error: {}", e),
+    };
+
+    println!("{:?}", &ast);
+
+    // Loop nodes and evaluate
+    let mut evaluator = Evaluator::new();
+    evaluator.evaluate(ast); 
+}
